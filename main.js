ba0771fc46cd1eda55ca34b9c441b71f1fb9e0be
Taehyun-Kiim <162668891+Taehyun-Kiim@users.noreply.github.com>
2024-03-21 16:51:32 +0900

    update

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 function update() {
-  //alert('클릭');
+  //   alert("update button clicked");
 
-  document.getElementById("location").innerHTML = "NEW YORK";
+  const cities = ["Seoul", "Tokyo", "Chicago", "Stuttgart"];
+  const num = parseInt(Math.random() * cities.length);
+  //   console.log(num);
+  //   document.getElementById("location").innerHTML = "Tokyo";
+  document.getElementById("location").innerHTML = cities[num];
+  document.getElementById("degree").innerHTML = parseInt(Math.random() * 30);
 }
-
-update();
